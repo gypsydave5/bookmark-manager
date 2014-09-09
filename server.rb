1dd5a81e4c76f1cb0922970b5244ca6ff9325d7b
James McNeil <james.r.mcneil@gmail.com>
2014-09-09 17:19:32 +0100

    Bookmark manager: added password enabled users

--- server.rb
+++ server.rb
@@ -7,6 +7,10 @@ DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
 
 require './lib/link' # this needs to be done after datamapper is initialised
 require './lib/tag' # this needs to be done after datamapper is initialised
+require './lib/user'
+
+enable :sessions
+set :sessions_secret, 'super secret'
 
 # After declaring your models, you should finalise them
 DataMapper.finalize
@@ -35,4 +39,27 @@ get '/tags/:text' do
 	@links = tag ? tag.links : []
 	erb :index
 end
+
+get '/users/new' do
+	# note the view is in views/users/new.erb
+	# we need the quotes because otherwise
+	# ruby would divide the symbol :users by the
+	# variable new (which makes no sense)
+	erb :"users/new"
+end
+
+post '/users' do
+	user = User.create(email: params[:email],
+				password: params[:password])
+	session[:user_id] = user.id
+	redirect to('/')
+end
+
+helpers do
+
+	def current_user
+		@current_user ||=User.get(session[:user_id]) if session[:user_id]
+	end
+end
+
 
