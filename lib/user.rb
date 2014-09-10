d8a3bd11436075629921e5709d75af374a3a807a
James McNeil <james.r.mcneil@gmail.com>
2014-09-10 11:11:53 +0100

    Bookmark manager: added user accounts and error messages to prevent user duplication

--- lib/user.rb
+++ lib/user.rb
@@ -8,7 +8,7 @@ class User
 	attr_accessor	:password_confirmation
 
 	property :id, Serial
-	property :email, String
+	property :email, String, unique: true, message: "This email is already taken"
 	# this will store both the password and the salt
 	# It's text and not String because String holds
 	# 50 characters by default
@@ -31,6 +31,7 @@ class User
 	# and password_confirmation are the same
 	# read more about it in the documentation
 	# http://datamapper.org/docs/validations.html
-	validates_confirmation_of :password
+	validates_confirmation_of 	:password, message: "Sorry, your passwords don't match"
+	# validates_uniqueness_of 	:email
 
 end
