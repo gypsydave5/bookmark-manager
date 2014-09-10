6ec5ab9c5c8bd03246ee0a3bb36cc603b68ba901
David Wickes <dave.wickes@gmail.com>
2014-09-10 12:46:55 +0100

    Adds feature for user signing out

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -66,3 +66,19 @@ feature "User sign in" do
 	end
 
 end
+
+feature 'User signs out' do
+	background(:each) do
+		User.create( email: "test@test.com",
+							   password: "test",
+							   password_confirmation: "test"	)
+	end
+
+	scenario 'while being signed in' do
+		sign_in('test@test.com', 'test')
+		click_button "Sign out"
+		expect(page).to have_content("Good bye!")
+		expect(page).not_to have_content("Welcome, test@test.com")
+	end
+
+end
