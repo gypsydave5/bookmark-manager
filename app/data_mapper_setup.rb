9e01793b8fe2d6381982b29b58921889b2ba8b36
David Wickes <dave.wickes@gmail.com>
2014-09-11 14:53:13 +0100

    changes some of the mailgun setup to pusg them over to environment variables. Refactors the datamapper config

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -1,11 +1,7 @@
 env = ENV["RACK_ENV"] || "development"
 # we're telling datamapper to use a postgres database on localhost. The name will be "bookmark_manager_test" or "bookmark_manager_development" depending on the environment
 
-if env =~ /(staging|production)/
-	DataMapper.setup(:default, ENV["DATABASE_URL"])
-else
-	DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
+DataMapper.setup(:default, ENV["DATABASE_URL"] || "postgres://localhost/bookmark_manager_#{env}")
 # After declaring your models, you should finalise them
-end
 
 DataMapper.finalize
--- app/helpers/mailgun.rb
+++ app/helpers/mailgun.rb
@@ -1,10 +1,12 @@
 API_KEY = ENV['MAILGUN_API_KEY']
-API_URL = "https://api:#{API_KEY}@api.mailgun.net/v2/app29460727.mailgun.org/messages"
+SMTP_LOGIN = ENV[MAILGUN_SMTP_LOGIN].split('@')[1]
+API_URL = "https://api:#{API_KEY}@#{SMTP_LOGIN}/messages"
+APP_URL = ENV["URL"]
 
 def send_email(email,token)
   RestClient.post API_URL,
   :from => "me@samples.mailgun.org",
   :to => email,
   :subject => "Bookmark Manager password reset",
-  :text => "Please click on the following likn to reset your password : http://www.shrouded-island-5985.herokuapp.com/sessions/recovery/#{token}"
+  :text => "Please click on the following link to reset your password : #{APP_URL}/sessions/recovery/#{token}"
 end
