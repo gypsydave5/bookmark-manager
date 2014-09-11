9923a73e0a9f4e48350ce3d08b1666fbf0390837
David Wickes <dave.wickes@gmail.com>
2014-09-11 17:01:01 +0100

    tried to fix the path for the email recovery link

--- app/helpers/mailgun.rb
+++ app/helpers/mailgun.rb
@@ -1,12 +1,12 @@
 API_KEY = ENV['MAILGUN_API_KEY']
 LOGIN = ENV['MAILGUN_SMTP_LOGIN'].split('@')[1]
 API_URL = "https://api:#{API_KEY}@api.mailgun.net/v2/#{LOGIN}/messages"
-APP_URL = ENV["URL"]
+APP_URL = ENV["APP_URL"]
 
 def send_email(email,token)
   RestClient.post API_URL,
   :from => "me@samples.mailgun.org",
   :to => email,
   :subject => "Bookmark Manager password reset",
-  :text => "Please click on the following link to reset your password : #{APP_URL}/sessions/recovery/#{token}"
+  :text => "Please click on the following link to reset your password : #{APP_URL}sessions/recovery/#{token}"
 end
