d35c6348124129a99ca75f3e68795f91c088caed
James McNeil <james.r.mcneil@gmail.com>
2014-10-29 11:00:24 +0000

    added new font

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -6,7 +6,7 @@ end
 post '/users' do
 	@user = User.create(email: params[:email],
 				password: params[:password],
-				:password_confirmation => params[:password_confirmation])
+				password_confirmation: params[:password_confirmation])
 	if @user.save
 		session[:user_id] = @user.id
 		redirect to('/')
